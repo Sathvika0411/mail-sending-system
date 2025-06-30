0f3b2b78fe8826ff7470f9e89820b2683868becc
sathvika <chamasathvika04@gmail.com>
2025-06-30 13:04:19 +0530

    Fix baseURL for deployed API

--- frontend/src/utils/axiosInstance.js
+++ frontend/src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8001/api",
+  baseURL: "https://mail-sending-system.onrender.com/api",
 });
 
 export default axiosInstance;
